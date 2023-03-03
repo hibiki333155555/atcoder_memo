5d7d066c350b23b11a653d04a9a37c6da21625da
MORI <105579842+hibiki333155555@users.noreply.github.com>
2023-03-03 23:20:39 +0900

    入茶したい

--- def.cpp
+++ def.cpp
@@ -4,6 +4,7 @@
 // #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+#define rep0(i,N) for(int i=0;i<N;i++)
 #define rep(i,a,b) for(int i=a;i<b;i++)
 #define rrep(i,a,b) for(int i=a;i>=b;i--)
 #define INT_MAX 2147483647
