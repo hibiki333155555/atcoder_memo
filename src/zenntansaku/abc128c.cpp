d51bc2b34424d2021f48b94cfcf5e45bbf697c56
MORI <105579842+hibiki333155555@users.noreply.github.com>
2023-03-03 21:42:34 +0900

    入茶したい

---
+++ src/zenntansaku/abc128c.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+// #include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define INT_MAX 2147483647
+
+int main() {
+    int N;
+    cin >> N;
+
+    return 0;
+}
