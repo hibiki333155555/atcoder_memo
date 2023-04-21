c55c44d15d169a670faf0ff4b3cd1bb195b4768b
MORI <105579842+hibiki333155555@users.noreply.github.com>
2023-04-21 18:13:17 +0900

    ?

--- def.cpp
+++ def.cpp
@@ -7,7 +7,7 @@ typedef long long ll;
 #define rep0(i,N) for(int i=0;i<N;i++)
 #define rep(i,a,b) for(int i=a;i<b;i++)
 #define rrep(i,a,b) for(int i=a;i>=b;i--)
-#define INT_MAX 2147483647
+
 
 int main() {
     int N;
