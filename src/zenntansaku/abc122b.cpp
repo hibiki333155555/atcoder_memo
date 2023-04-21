e4a99ea7e159da74ede1b3c30d037453e90443c8
HIBIKI <105579842+hibiki333155555@users.noreply.github.com>
2023-04-21 18:09:07 +0900

    Update abc122b.cpp

--- src/zenntansaku/abc122b.cpp
+++ src/zenntansaku/abc122b.cpp
@@ -1,20 +1,28 @@
 #include <iostream>
 #include <string>
 using namespace std;
+string T = "ATCG";
 
-
+// 入力された文字列の部分文字列のうちACTGだけを含むもので最長の文字列の長さを求める。
+// 入力サイズが小さいので全ての場合について探索できる
+// ある部分文字列がAGCTだけを含む文字列か判定するにはstring の find method を使う
 int main() {
     string S;
     cin >> S;
-    int N;
-    N = S.length();
+    int N; N = S.length();
 
-    int count = 0;
+    int ans = 0;
 
-    for(int i = 0; i < N; i++) {
-        if(S[i] == 'A' && S[i + 1] == 'B' && S[i + 2] == 'C') count++;
+    for(int l = 0; l < N; l++) {
+        for(int r = l; r < N; r++) {
+            bool flag = true;
+            // str.find(x) でstrにxが含まれていたらxがあるstrの添字番号の先頭を返す
+            // xが含まれていない場合nposを返す
+            for(int x = l; x <= r; x++) if(T.find(S[x]) == string::npos) flag = false;
+            if(flag) ans = max(ans, r - l + 1);
+        }
     }
 
-    cout << count << endl;
+    cout << ans << "\n";
     return 0;
 }
